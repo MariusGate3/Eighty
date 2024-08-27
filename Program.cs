1334a944bd31430a34284d04714ac149a8185d0a
MariusGate3 <54096802+MariusGate3@users.noreply.github.com>
2024-08-27 20:32:22 +0200

    Eighty finished

--- Program.cs
+++ Program.cs
@@ -2,4 +2,20 @@
     Console.WriteLine("Please provide a filename.");
     return 1;
 }
-return 0;
+
+try {
+    string[] lines = File.ReadAllLines(args[0]);
+    
+    
+    for (int i = 0; i < lines.Length; i++) {
+        int len = lines[i].Length;
+        if (len > 80) {
+            Console.WriteLine("Line %d is too long (%d characters)", i, len);
+        }
+    }
+    return 0;
+}
+catch {
+    Console.WriteLine("Could not open file.");
+    return 1;
+}
--- obj/Debug/net8.0/eighty.AssemblyInfo.cs
+++ obj/Debug/net8.0/eighty.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eighty")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3da3ec2c33355447cf789eb0af4fbbe5041682f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("eighty")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eighty")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
