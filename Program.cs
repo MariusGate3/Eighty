dee9c155b01cbea9d2e1acbd5ecebd2cafcd6392
MariusGate3 <54096802+MariusGate3@users.noreply.github.com>
2024-08-27 20:47:18 +0200

    Possible to specify the maximum length of lines, by adding a max length after the file to check

--- Program.cs
+++ Program.cs
@@ -5,12 +5,13 @@
 
 try {
     string[] lines = File.ReadAllLines(args[0]);
+    int maxLen = (int.TryParse(args[args.Length - 1], out int maxInput)) ? maxInput : 80;
     
     
     for (int i = 0; i < lines.Length; i++) {
         int len = lines[i].Length;
         if (len > 80) {
-            Console.WriteLine("Line %d is too long (%d characters)", i, len);
+            Console.WriteLine("Line {0} is too long ({1} characters, maximum is {2})", i, len, maxLen);
         }
     }
     return 0;
